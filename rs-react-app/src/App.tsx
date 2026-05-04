4f52a921b90ef6a07a0cd62b3960c0649efab5b4
Kate Kurakina <katevolmont@gmail.com>
2026-05-04 23:32:59 +0300

    feat: add error boundary with fallback UI and test button

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from './components/Header/Header';
 import Results from "./components/Results/Results";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
 type State = {
   search: string;
 }
+
+
 
 class App extends React.Component<object, State> {
   state: State = {
@@ -15,15 +18,26 @@ class App extends React.Component<object, State> {
     this.setState({ search: value });
   }
 
+    throwError = () => {
+    throw new Error('Test error');
+  }
+
+
   render() {
     return (
-      <div className="app">
-        <Header
-          onSearch={this.handleSearch}
-          currentSearch={this.state.search}
-        />
-        <Results search={this.state.search} />
-      </div>
+        <ErrorBoundary>
+          <div className="app">
+            <Header
+              onSearch={this.handleSearch}
+              currentSearch={this.state.search}
+            />
+            <Results search={this.state.search} />
+
+            <button onClick={this.throwError}>
+              Test Error
+            </button>
+          </div>
+        </ErrorBoundary>
     );
   }
 }
