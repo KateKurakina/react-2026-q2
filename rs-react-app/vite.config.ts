845eeb23748e22a09bcd3abbec885594450a7529
Kate Kurakina <katevolmont@gmail.com>
2026-05-11 22:42:12 +0300

    feat: configure vitest and testing library

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -1,8 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
 
-// https://vite.dev/config/
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   base: '/react-2026-q2',
   plugins: [react()],
-})
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+
+    coverage: {
+      provider: 'v8',
+
+      include: ['src/**/*.{ts,tsx}'],
+
+      exclude: [
+        'src/main.tsx',
+        'src/setupTests.ts',
+        'src/**/*.test.{ts,tsx}',
+        'src/**/*.spec.{ts,tsx}',
+        'src/**/*.d.ts',
+      ],
+
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
+});
