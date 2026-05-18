996d638d76b2056c0197dcc144b33c33b036b663
Kate Kurakina <katevolmont@gmail.com>
2026-05-18 23:11:00 +0300

    refactor: move app state to Home component

--- rs-react-app/src/pages/Home.tsx
+++ rs-react-app/src/pages/Home.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+
+import Header from "../components/Header/Header";
+import Results from "../components/Results/Results";
+import ErrorTrigger from "../components/ErrorTrigger/ErrorTrigger";
+
 export default function Home() {
-    return <h1>Home</h1>
+  const [search, setSearch] = useState('');
+  const [hasCrash, setHasCrash] = useState(false);
+
+  const handleSearch = (value: string) => {
+    setSearch(value);
+  };
+
+  return (
+    <div className="app">
+
+      <Header
+        onSearch={handleSearch}
+        currentSearch={search}
+      />
+
+      <Results search={search} />
+
+      <button
+        className="button__error"
+        onClick={() => setHasCrash(true)}
+      >
+        Test Error
+      </button>
+
+      <ErrorTrigger shouldCrash={hasCrash} />
+
+    </div>
+  );
 }
