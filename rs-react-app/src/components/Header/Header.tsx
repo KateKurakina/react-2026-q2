08ea24ac26969a0ac97545085b160a141046cfaf
Kate Kurakina <katevolmont@gmail.com>
2026-05-18 23:00:39 +0300

    refactor: convert Header to functional component

--- rs-react-app/src/components/Header/Header.tsx
+++ rs-react-app/src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Search from '../Search/Search'
 
 type Props = {
@@ -6,17 +5,16 @@ type Props = {
     currentSearch: string;
 }
 
-class Header extends React.Component<Props> {
-    render() {
-        return (
-            <header className="header">
-                <Search
-                    onSearch={this.props.onSearch}
-                    currentSearch={this.props.currentSearch}
-                />
-            </header>
-        );
-    }
+export default function Header({
+    onSearch,
+    currentSearch,
+}: Props)  {
+    return (
+        <header className="header">
+            <Search
+                onSearch={onSearch}
+                currentSearch={currentSearch}
+            />
+        </header>
+    );
 }
-
-export default Header;
