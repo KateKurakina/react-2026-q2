357f5c80338242a3adf48342a045d01affc647af
Kate Kurakina <katevolmont@gmail.com>
2026-05-24 12:30:03 +0300

    feat: add checkbox selection for pokemon cards

--- rs-react-app/src/components/Card/Card.tsx
+++ rs-react-app/src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { useSelectedStore } from "../../store/selectedStore";
 import type { PokemonItem } from "../../types";
 
 type Props = {
@@ -7,9 +8,19 @@ type Props = {
 
 export default function Card({ item }: Props) {   
     const navigate = useNavigate();
+
+    const { selected, toggleSelected } = useSelectedStore();
+
+    const checked = selected.includes(item.name);
     
     return (
         <div className="card" onClick={() => navigate(`/${item.name}`)}>
+            <input 
+            type="checkbox" 
+            checked={checked} 
+            onClick={(e) => e.stopPropagation()}
+            onChange={() => toggleSelected(item.name)}
+            />
             <h3>{item.name}</h3>
             <p>{item.description}</p>
         </div>
